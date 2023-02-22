ac7fbcc472836d221b3a7f074bcd0b35c7452def
ClaireFree <claire@robingoodfoods.com>
2023-02-22 14:49:52 -0800

    added rows to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,10 @@ const Home = () => {
     <div>
         <Main/>
         <Row title='UpComing' fetchURL={requests.requestUpcoming}/>
+        <Row title='Popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row title='Horror' fetchURL={requests.requestHorror} />
     </div>
   )
 }
