56c1644869ede261f7ff0c33a1e38265caf164a0
ClaireFree <claire@robingoodfoods.com>
2023-02-23 00:16:08 -0800

    fixed styling issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,21 +30,21 @@ const Login = () => {
     <br/><br/>
     <div className='bg-gray/60 fixed top-0 left-0 w-full h-screen '></div>
     <div className='fixed w-full px-4 py-24 z-50 '>
-      <div className='max-w-[450px] h-[600px] mx-auto bg-neutral-800/75 text-white rounded-md'>
+      <div className='max-w-[450px] h-[600px] mx-auto  bg-black/75 text-white rounded-md'>
         <div className='max-w-[320px] mx-auto py-16'>
           <h1 className='text-3xl font-bold'>Login</h1>
           {error ? <p className='p-3 my-2'>{error}</p> : null}
           <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
             <input
               onChange={(e) => setEmail(e.target.value)}
-              className='p-3 my-2 bg-gray-700 rounded-md'
+              className='p-3 my-2 text-black bg-white rounded-md'
               type='email'
               placeholder='Email'
               autoComplete='email'
             />
             <input
               onChange={(e) => setPassword(e.target.value)}
-              className='p-3 my-2 bg-gray-700 rounded-md'
+              className='p-3 my-2  text-black bg-white  rounded-md'
               type='password'
               placeholder='Password'
               autoComplete='current-password'
